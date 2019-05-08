5a7baf93de0d1c7d8c708784f99735fd50c22814
rchaliuk <rchaliuk@oroinc.com>
2019-05-08 18:10:27 +0300

    BAP-18813: Add collector to get current user info (#23788)
     - added parameters to attributes bag
     - updated user detail and page variable collectors
     - added unit tests

--- src/Oro/Bundle/GoogleTagManagerBundle/Tests/Unit/DataLayer/Collector/PageVariablesCollectorTest.php
+++ src/Oro/Bundle/GoogleTagManagerBundle/Tests/Unit/DataLayer/Collector/PageVariablesCollectorTest.php
@@ -4,12 +4,21 @@ namespace Oro\Bundle\GoogleTagManagerBundle\Tests\Unit\DataLayer\Collector;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Oro\Bundle\GoogleTagManagerBundle\DataLayer\Collector\PageVariablesCollector;
+use Oro\Bundle\GoogleTagManagerBundle\DataLayer\ConstantBag\DataLayerAttributeBag;
 use Oro\Bundle\GoogleTagManagerBundle\Provider\PageTypeProvider;
+use Oro\Bundle\LocaleBundle\Entity\Localization;
+use Oro\Bundle\LocaleBundle\Helper\LocalizationHelper;
+use Oro\Component\Testing\Unit\EntityTrait;
 
 class PageVariablesCollectorTest extends \PHPUnit\Framework\TestCase
 {
+    use EntityTrait;
+
     /** @var PageTypeProvider|\PHPUnit\Framework\MockObject\MockObject */
     private $pageTypeProvider;
+
+    /** @var LocalizationHelper|\PHPUnit\Framework\MockObject\MockObject */
+    private $localizationHelper;
 
     /** @var PageVariablesCollector */
     private $collector;
@@ -17,27 +26,33 @@ class PageVariablesCollectorTest extends \PHPUnit\Framework\TestCase
     protected function setUp()
     {
         $this->pageTypeProvider = $this->createMock(PageTypeProvider::class);
+        $this->localizationHelper = $this->createMock(LocalizationHelper::class);
 
-        $this->collector = new PageVariablesCollector($this->pageTypeProvider);
+        $this->collector = new PageVariablesCollector($this->pageTypeProvider, $this->localizationHelper);
     }
 
     /**
      * @dataProvider handleDataProvider
      *
-     * @param string|null $testType
+     * @param string|null $type
+     * @param Localization|null $localization
      * @param array $expected
      */
-    public function testHandle(?string $testType, array $expected): void
+    public function testHandle(?string $type, ?Localization $localization, array $expected): void
     {
         $this->pageTypeProvider->expects($this->once())
             ->method('getType')
-            ->willReturn($testType);
+            ->willReturn($type);
+
+        $this->localizationHelper->expects($this->once())
+            ->method('getCurrentLocalization')
+            ->willReturn($localization);
 
         $data = new ArrayCollection();
 
         $this->collector->handle($data);
 
-        $this->assertEquals($expected, $data[0]);
+        $this->assertEquals($expected, $data->toArray());
     }
 
     /**
@@ -45,20 +60,26 @@ class PageVariablesCollectorTest extends \PHPUnit\Framework\TestCase
      */
     public function handleDataProvider(): array
     {
+        $localization = $this->getEntity(Localization::class, ['id' => 42]);
+
         return [
             'with type' => [
                 'type' => 'test_type',
+                'localization' => $localization,
                 'expected' => [
-                    'page' => [
-                        'type' => 'test_type',
+                    [
+                        DataLayerAttributeBag::KEY_PAGE_TYPE => 'test_type',
+                        DataLayerAttributeBag::KEY_LOCALIZATION_ID => '42',
                     ]
                 ]
             ],
             'without type' => [
                 'type' => null,
+                'localization' => null,
                 'expected' => [
-                    'page' => [
-                        'type' => null,
+                    [
+                        DataLayerAttributeBag::KEY_PAGE_TYPE => null,
+                        DataLayerAttributeBag::KEY_LOCALIZATION_ID => null,
                     ]
                 ]
             ],
