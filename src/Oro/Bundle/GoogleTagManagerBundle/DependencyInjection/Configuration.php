f344869f778b32013652355ded6057d5488cdd2c
Mykhailo Sulyma <msulima@oroinc.com>
2019-12-04 19:14:57 +0200

    BAP-19397: Upgrade to Symfony 4.4 (#25955)
     - added AuthenticatedTokenTrait to return old logic of tokens when list of user roles was changed
     - Deprecated constructing a `TreeBuilder` without passing root node information
     - added Symfony 4.3 automatic validation
     - The getExtendedType() method of the FormTypeExtensionInterface is deprecated and will be removed in 5.0
     - The Kernel::getRootDir() and the kernel.root_dir parameter have been deprecated
     - Deprecated the Process::setCommandline() and the PhpProcess::setPhpBinary() methods
     - The Translator::transChoice() method has been deprecated in favor of using Translator::trans() with "%count%" as the parameter driving plurals
     - The MimeTypeGuesserInterface and ExtensionGuesserInterface interfaces have been deprecated, use Symfony\Component\Mime\MimeTypesInterface instead.
     - The MimeType and MimeTypeExtensionGuesser classes have been deprecated, use Symfony\Component\Mime\MimeTypes instead.
     - Deprecated ResourceBundle namespace
     - Deprecated Intl::getCurrencyBundle(), use Currencies instead
     - Deprecated Intl::getLanguageBundle(), use Languages or Scripts instead
     - Deprecated Intl::getLocaleBundle(), use Locales instead
     - Deprecated Intl::getRegionBundle(), use Countries instead
     - Deprecated RegistryInterface, use Doctrine\Common\Persistence\ManagerRegistry
    - updated composer.lock files to symfony 4.4.1
    - updated dev.json
    - updated composer.json

--- src/Oro/Bundle/GoogleTagManagerBundle/DependencyInjection/Configuration.php
+++ src/Oro/Bundle/GoogleTagManagerBundle/DependencyInjection/Configuration.php
@@ -13,8 +13,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('oro_google_tag_manager');
+        $treeBuilder = new TreeBuilder('oro_google_tag_manager');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
