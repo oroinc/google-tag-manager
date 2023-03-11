cadaf1105e2dbf92746da2d286133c7cc871e1b0
Vova Soroka <vsoroka@oroinc.com>
2023-03-11 17:06:47 +0200

    BAP-21930: Refactor DI extensions for bundles (#35041)
    - add getSettings() method to Oro\Bundle\ConfigBundle\DependencyInjection\SettingsBuilder that simplifies loading of system settings into DIC in DI extension
    - refactor loading of system settings into DIC in DI extensions
    - add strict type to DI extensions
    - refactor unit tests for DI extensions

--- src/Oro/Bundle/GoogleTagManagerBundle/DependencyInjection/OroGoogleTagManagerExtension.php
+++ src/Oro/Bundle/GoogleTagManagerBundle/DependencyInjection/OroGoogleTagManagerExtension.php
@@ -2,6 +2,7 @@
 
 namespace Oro\Bundle\GoogleTagManagerBundle\DependencyInjection;
 
+use Oro\Bundle\ConfigBundle\DependencyInjection\SettingsBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
@@ -10,12 +11,12 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 class OroGoogleTagManagerExtension extends Extension
 {
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $config = $this->processConfiguration(new Configuration(), $configs);
-        $container->prependExtensionConfig($this->getAlias(), array_intersect_key($config, array_flip(['settings'])));
+        $config = $this->processConfiguration($this->getConfiguration($configs, $container), $configs);
+        $container->prependExtensionConfig($this->getAlias(), SettingsBuilder::getSettings($config));
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('collectors.yml');
--- src/Oro/Bundle/GoogleTagManagerBundle/Tests/Unit/DependencyInjection/OroGoogleTagManagerExtensionTest.php
+++ src/Oro/Bundle/GoogleTagManagerBundle/Tests/Unit/DependencyInjection/OroGoogleTagManagerExtensionTest.php
@@ -3,45 +3,45 @@
 namespace Oro\Bundle\GoogleTagManagerBundle\Tests\Unit\DependencyInjection;
 
 use Oro\Bundle\GoogleTagManagerBundle\DependencyInjection\OroGoogleTagManagerExtension;
-use Oro\Bundle\TestFrameworkBundle\Test\DependencyInjection\ExtensionTestCase;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
-class OroGoogleTagManagerExtensionTest extends ExtensionTestCase
+class OroGoogleTagManagerExtensionTest extends \PHPUnit\Framework\TestCase
 {
-    private ContainerBuilder $container;
-
-    private OroGoogleTagManagerExtension $extension;
-
-    protected function setUp(): void
-    {
-        $this->container = new ContainerBuilder();
-
-        $this->extension = new OroGoogleTagManagerExtension();
-    }
-
-    public function testGetAlias(): void
-    {
-        self::assertEquals('oro_google_tag_manager', $this->extension->getAlias());
-    }
-
     public function testLoad(): void
     {
-        $this->extension->load([], $this->container);
+        $container = new ContainerBuilder();
 
-        self::assertEquals([
+        $extension = new OroGoogleTagManagerExtension();
+        $extension->load([], $container);
+
+        self::assertNotEmpty($container->getDefinitions());
+        self::assertSame(
             [
-                'settings' => [
-                    'resolved' => true,
-                    'integration' => [
-                        'value' => null,
-                        'scope' => 'app',
-                    ],
-                    'enabled_data_collection_types' => [
-                        'value' => ['google_analytics4'],
-                        'scope' => 'app',
-                    ],
-                ],
+                [
+                    'settings' => [
+                        'resolved' => true,
+                        'integration' => ['value' => null, 'scope' => 'app'],
+                        'enabled_data_collection_types' => ['value' => ['google_analytics4'], 'scope' => 'app']
+                    ]
+                ]
             ],
-        ], $this->container->getExtensionConfig('oro_google_tag_manager'));
+            $container->getExtensionConfig('oro_google_tag_manager')
+        );
+
+        self::assertSame(30, $container->getParameter('oro_google_tag_manager.products.batch_size'));
+    }
+
+    public function testLoadWithCustomConfigs(): void
+    {
+        $container = new ContainerBuilder();
+
+        $configs = [
+            ['config' => ['batch_size' => 100]]
+        ];
+
+        $extension = new OroGoogleTagManagerExtension();
+        $extension->load($configs, $container);
+
+        self::assertSame(100, $container->getParameter('oro_google_tag_manager.products.batch_size'));
     }
 }
